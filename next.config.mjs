439d2cf308622a7f828459eeab8b3798c48c15d7
Nada Magdy badr <nadabadr236@gmail.com>
2025-07-20 23:40:24 +0300

    update2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/home",
-        destination: "/",
-      },
-    ];
+  images: {
+    domains: ["ik.imagekit.io"],
   },
 };
 
