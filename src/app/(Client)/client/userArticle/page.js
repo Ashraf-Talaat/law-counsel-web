cd3fb6440b5e5583cf85be49da66fb62a211e6a4
Zahra172 <ZaHra_magdi@protonmail.com>
2025-07-22 23:29:30 +0300

    edit user article

--- src/app/(Client)/client/userArticle/page.js
+++ src/app/(Client)/client/userArticle/page.js
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import HeroOther from "@/_components/layout/hero-other";
-export default function page() {
+import { fetchArticles } from "@/services/client/getAllArticles";
+
+export default function Page() {
+  let [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const allArticle = async () => {
+      const data = await fetchArticles();
+      console.log(data);
+      setArticles(data);
+    };
+    allArticle();
+  }, []);
+
   return (
     <>
       <HeroOther
@@ -11,39 +25,42 @@ export default function page() {
       />
       <div className="w-[85%] mx-auto my-20">
         <p>المقالات الاكثر انتشارا</p>
-        <div className="w-[60%] mx-auto my-20 p-6 bg-white ">
-          {[1, 2, 3].map((_, idx) => (
-            <div key={idx} className="mb-15 p-8 rounded-lg shadow-lg">
-              <div className="flex w-[20%] justify-between mb-6 ">
-                {/* ternary operator to check if the image exists */}
+        <div className="w-full max-w-3xl mx-auto my-10 p-6 bg-white ">
+          {articles.map((art) => (
+            <div key={art.id} className="mb-15 p-8 rounded-lg shadow-lg">
+              <div className="flex items-center mb-6">
                 <Image
-                  src="/images/avatar.png"
+                  src="/images/logo-dark.png"
                   alt="topRated"
                   width={50}
                   height={50}
-                  className="rounded-full"
+                  className="rounded-full mr-4"
                 />
+
                 <div>
-                  <p className="font-semibold mb-2">ندي محمد</p>
-                  <p className="text-sm">اليوم</p>
+                  <p className="font-semibold mb-1">ندي محمد</p>
+                  <p className="text-sm text-gray-500">
+                    {new Date(art.createdAt).toLocaleDateString("ar-EG", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
                 </div>
               </div>
-              <div className="my-4">
-                <p className="text-sm text-gray-600">
-                  المقالات القانونية هي مقالات تتناول مواضيع قانونية متنوعة، مثل
-                  حقوق الإنسان، القانون الجنائي، القانون المدني، وغيرها. تهدف
-                  هذه المقالات إلى توعية الجمهور بالقوانين والحقوق والواجبات
-                  القانونية. المقالات القانونية هي مقالات تتناول مواضيع قانونية
-                  متنوعة، مثل حقوق الإنسان، القانون الجنائي، القانون المدني،
-                  وغيرها. تهدف هذه المقالات إلى توعية الجمهور بالقوانين والحقوق
-                  والواجبات القانونية. المقالات القانونية هي مقالات تتناول
-                  مواضيع قانونية متنوعة، مثل حقوق الإنسان، القانون الجنائي،
-                  القانون المدني، وغيرها. تهدف هذه المقالات إلى توعية الجمهور
-                  بالقوانين والحقوق والواجبات القانونية. المقالات القانونية هي
-                  مقالات تتناول مواضيع قانونية متنوعة، مثل حقوق الإنسان، القانون
-                  الجنائي، القانون المدني، وغيرها. تهدف هذه المقالات إلى توعية
-                  الجمهور بالقوانين والحقوق والواجبات القانونية.
-                </p>
+
+              <div className="mb-4">
+                <Image
+                  src={art.imageUrl}
+                  alt={art.title ? art.title : ""}
+                  width={900}
+                  height={100}
+                  className="rounded-md"
+                />
+              </div>
+
+              <div>
+                <p className="text-sm text-gray-600">{art.content}</p>
               </div>
             </div>
           ))}
