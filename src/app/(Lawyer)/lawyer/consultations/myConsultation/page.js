7a8f02a68648ea2ed76674fe4fc8e2a3d50e0990
Ahmed Mossad <mossadahmed975@gmail.com>
2025-08-02 20:06:48 +0300

    create chat

--- src/app/(Lawyer)/lawyer/consultations/myConsultation/page.js
+++ src/app/(Lawyer)/lawyer/consultations/myConsultation/page.js
@@ -1,10 +1,29 @@
+'use client'
+
 import Image from "next/image";
-import React from "react";
 
 //Icons
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getAllChatsRealtime } from "@/logic/consultations/lawyer/getAllChats";
+import index from "@/_components/common/categoryBtn";
+
 
 export default function page() {
+  const [chats, setChats] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [index, setIndex] = useState(0);
+
+
+  useEffect(() => {
+    const unsubscribe = getAllChatsRealtime("5oUmpSlGVTX2AsQoSlUpsUsWxi83", (chat) => {
+      setChats(chat);
+      setMessages(chat[index].messages)
+    });
+
+    return () => unsubscribe(); // تنظيف عند الخروج من الصفحة
+  }, []);
   return (
     <>
       <div className="bg-white rounded-md shadow-md p-6 w-[85%] mx-auto ">
@@ -13,9 +32,13 @@ export default function page() {
           <div className="w-1/2 p-5 rounded-md bg-gray-100 shadow-md ">
             <h2 className="text-xl font-bold mb-4 goldTxt">العملاء</h2>
             <ul className="space-y-4">
-              {[1, 2, 3].map((id) => (
+              {chats.map((item, i) => (
                 <li
-                  key={id}
+                  onClick={() => {
+                    setIndex(i)
+                    setMessages(item.messages)
+                  }}
+                  key={item.clientId}
                   className="flex items-center gap-3 p-3 bg-white rounded-md cursor-pointer hover:bg-gray-200 "
                 >
                   <Image
@@ -28,7 +51,7 @@ export default function page() {
 
                   <div>
                     <p className="font-semibold mb-1">اسم العميل</p>
-                    <p className="text-sm subTxt ">عنوان الاستشارة</p>
+                    <p className="text-sm subTxt "> {item.lastMessage}</p>
                   </div>
                 </li>
               ))}
@@ -36,34 +59,58 @@ export default function page() {
           </div>
 
           {/* chat */}
-          <div className="w-1/2 h-[500px] p-6 flex flex-col justify-between rounded-md shadow-2xl">
+          {/* <div className="w-1/2 h-[500px] p-6 flex flex-col justify-between rounded-md shadow-2xl">
             <div>
               <h2 className="text-xl font-bold mb-4 goldTxt">الشات</h2>
               <div className="space-y-3">
-                <div className=" bg-gray-200 p-3 rounded-md w-fit ms-auto">
-                  مرحبًا، عندي استشارة قانونية بخصوص...
-                </div>
-                <div className="text-left bg-blue-200 p-3 rounded-md w-fit">
-                  تفضل، احكيلي التفاصيل
-                </div>
+                {messages.map((item) => (
+                  item.senderId == "5oUmpSlGVTX2AsQoSlUpsUsWxi83" ?
+                    <div className="text-left bg-blue-200 p-3 rounded-md w-fit">
+                      {item.message}
+                    </div> :
+                    <div className=" bg-gray-200 p-3 rounded-md w-fit ms-auto">
+                      {item.message}
+                    </div>
+                ))
+                }
+              </div>
+            </div> */}
+          <div className="w-1/2 h-[500px] p-6 flex flex-col justify-between rounded-md shadow-2xl">
+            <div>
+              <h2 className="text-xl font-bold mb-4 goldTxt">الشات</h2>
+
+              {/* ✅ Scrollable container */}
+              <div className="space-y-1 overflow-y-auto h-[350px] pr-2 scroll-hidden">
+                {messages.map((item) => (
+                  item.senderId === "5oUmpSlGVTX2AsQoSlUpsUsWxi83" ? (
+                    <div key={item.id} className="text-left bg-blue-200 p-3 rounded-md w-fit">
+                      {item.message}
+                    </div>
+                  ) : (
+                    <div key={item.id} className="bg-gray-200 p-3 rounded-md w-fit ms-auto">
+                      {item.message}
+                    </div>
+                  )
+                ))}
               </div>
             </div>
+          
 
-            {/* كتابة رسالة */}
-            <div className="mt-6 flex">
-              <input
-                type="text"
-                placeholder="اكتب رسالتك هنا"
-                className="w-full border p-3 rounded-md focus:outline-none focus:ring-1 focus:ring-[#c9b38c] caret-[#c9b38c] text-gray-700"
-                style={{ borderColor: "#c9b38c" }}
-              />
-              <button className="p-2 goldTxt hover:bg-[#c9b38c36] rounded rotate-180 ms-2">
-                <PaperAirplaneIcon className="w-9 h-9" />
-              </button>
-            </div>
+          {/* كتابة رسالة */}
+          <div className="mt-6 flex">
+            <input
+              type="text"
+              placeholder="اكتب رسالتك هنا"
+              className="w-full border p-3 rounded-md focus:outline-none focus:ring-1 focus:ring-[#c9b38c] caret-[#c9b38c] text-gray-700"
+              style={{ borderColor: "#c9b38c" }}
+            />
+            <button className="p-2 goldTxt hover:bg-[#c9b38c36] rounded rotate-180 ms-2">
+              <PaperAirplaneIcon className="w-9 h-9" />
+            </button>
           </div>
         </div>
       </div>
+    </div >
     </>
   );
 }
