9dbc4796c82a7e502d43c8917b0f6b18e5a4916f
Ahmed Mossad <mossadahmed975@gmail.com>
2025-08-12 00:06:45 +0300

    solve bug of Ashraf

--- src/app/(Client)/client/consultations/myRequests/page.js
+++ src/app/(Client)/client/consultations/myRequests/page.js
@@ -15,11 +15,7 @@ import Swal from "sweetalert2";
 //components
 import editClientRequest from "@/logic/consultations/client/editClientRequest";
 import EditModal from "@/_components/editClientRequest/EditModal";
-
 import LoadingLogo from "@/_components/Loading";
-
-import FeedbackForm from "@/_components/FeedbackForm";
-
 
 export default function Page() {
   const [requests, setRequests] = useState([]);
@@ -87,60 +83,26 @@ export default function Page() {
   };
 
   // UI
-
   if (isLoading) {
     return <LoadingLogo />;
   } else {
- 
-  if (requests.length === 0) {
-    return (
-      <div className="bg-white rounded-md shadow-md p-6 w-[85%] mx-auto ">
-        <h2 className="text-xl font-bold mb-6 goldTxt">طلباتي</h2>
-        <p>لا توجد طلبات</p>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <div className="bg-white rounded-md shadow-md p-6 w-[85%] mx-auto ">
-        <h2 className="text-xl font-bold mb-6 goldTxt">طلباتي</h2>
-        <div className="space-y-6">
-          <FeedbackForm/>
-          {requests.map((item) => (
-            <div key={item.id} className="rounded-md p-4 bg-gray-100 shadow-md">
-              <div className="flex justify-between items-center mb-2">
-                <p className="font-semibold">عنوان الاستشارة : {item.title}</p>
-                <span className="text-sm text-white bg-yellow-500 px-2.5 p-1.5 rounded-full">
-                  {item.status === "pending" ? "قيد الانتظار" : "تم الانتهاء"}
-                </span>
-              </div>
-              <p className="text-gray-700 mb-3">
-                محتوى الطلب: {item.description}
-              </p>
-              <p className="font-semibold">اسم المحامي: {item.nameLawyer}</p>
-
-              {/* buttons */}
-              <div className="flex justify-end gap-4">
-                {/* Edit button */}
-                <button
-                  onClick={() => {
-                    setSelectedRequest(item);
-                    setIsModalOpen(true);
-                  }}
-                  type="button"
-                  className="flex items-center gap-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                >
-                  <PencilIcon className="w-5 h-5" />
-                  تعديل
-                </button>
-
-                {/* Delete button */}
-                <button
-                  onClick={() => handleDeleteRequest(item.id)}
-                  type="button"
-                  className="flex items-center gap-2 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-
+    if (requests.length === 0) {
+      return (
+        <div className="bg-white rounded-md shadow-md p-6 w-[85%] mx-auto ">
+          <h2 className="text-xl font-bold mb-6 goldTxt">طلباتي</h2>
+          <p>لا توجد طلبات</p>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className="bg-white rounded-md shadow-md p-6 w-[85%] mx-auto ">
+            <h2 className="text-xl font-bold mb-6 goldTxt">طلباتي</h2>
+            <div className="space-y-6">
+              {requests.map((item) => (
+                <div
+                  key={item.id}
+                  className="rounded-md p-4 bg-gray-100 shadow-md"
                 >
                   <div className="flex justify-between items-center mb-2">
                     <p className="font-semibold">
